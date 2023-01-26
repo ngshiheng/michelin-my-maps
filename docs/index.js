17123fb74a79aa42e1eaeea73c1d261d977f5e2b
Jerry Ng <ngshiheng@gmail.com>
2023-01-26 21:40:14 +0800

    refactor: read data with import

--- docs/index.js
+++ docs/index.js
@@ -5,15 +5,13 @@ import {
     formatNanoseconds,
 } from "https://unpkg.com/@lyrasearch/lyra@0.3.1/dist/esm/src/lyra.js";
 
+import data from "./data.json" assert { type: "json" };
+
 let restaurantDB;
 const summary = document.getElementById("search-summary");
 const table = document.getElementById("search-results");
 
 async function createLyraInstance(event) {
-    const endpoint = "data.json";
-    const response = await fetch(endpoint);
-    const data = await response.json();
-
     restaurantDB = create({
         schema: {
             Name: "string",
@@ -75,7 +73,9 @@ function jsonToHtmlTable(json) {
                 case "Name": {
                     const websiteUrl = element.document["WebsiteUrl"];
                     if (!websiteUrl) {
-                        const text = document.createTextNode(element.document[key]);
+                        const text = document.createTextNode(
+                            element.document[key]
+                        );
                         cell.appendChild(text);
                         break;
                     }
@@ -89,7 +89,10 @@ function jsonToHtmlTable(json) {
                     const address = element.document["Address"];
                     if (address) {
                         const link = document.createElement("a");
-                        link.setAttribute("href", `https://maps.google.com/?q=${address}`);
+                        link.setAttribute(
+                            "href",
+                            `https://maps.google.com/?q=${address}`
+                        );
                         link.setAttribute("target", "_blank");
                         link.innerText = address;
                         cell.appendChild(link);
@@ -99,7 +102,9 @@ function jsonToHtmlTable(json) {
                 case "PhoneNumber": {
                     const phoneNumber = element.document["PhoneNumber"];
                     if (phoneNumber) {
-                        let text = document.createTextNode(element.document[key]);
+                        let text = document.createTextNode(
+                            element.document[key]
+                        );
                         cell.appendChild(text);
                     }
                     break;
@@ -143,7 +148,7 @@ function handleSearch(event) {
     jsonToHtmlTable(searchResult.hits);
 }
 
-window.onload = async function(event) {
+window.onload = async function (event) {
     await createLyraInstance(event);
     handleSearch(event);
 };
