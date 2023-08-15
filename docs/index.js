0e1275009cc04877456c6a9a8151ff3c6997f6fc
Jerry Ng <ngshiheng@gmail.com>
2023-08-15 12:18:54 +0800

    style: hide phone number

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,8 @@
 import {
-    create,
-    search,
-    insertBatch,
-    formatNanoseconds,
+  create,
+  search,
+  insertBatch,
+  formatNanoseconds,
 } from "https://unpkg.com/@lyrasearch/lyra@0.3.1/dist/esm/src/lyra.js";
 
 let restaurantDB;
@@ -10,149 +10,145 @@ const summary = document.getElementById("search-summary");
 const table = document.getElementById("search-results");
 
 async function createLyraInstance(event) {
-    const endpoint = "data.json";
-    const response = await fetch(endpoint);
-    const data = await response.json();
+  const endpoint = "data.json";
+  const response = await fetch(endpoint);
+  const data = await response.json();
 
-    restaurantDB = create({
-        schema: {
-            Name: "string",
-            Address: "string",
-            Location: "string",
-            Price: "string",
-            Cuisine: "string",
-            Longitude: "string",
-            Latitude: "string",
-            PhoneNumber: "string",
-            Url: "string",
-            WebsiteUrl: "string",
-            Award: "string",
-            FacilitiesAndServices: "string",
-        },
-    });
-    await insertBatch(restaurantDB, data, { batchSize: 100 });
+  restaurantDB = create({
+    schema: {
+      Name: "string",
+      Address: "string",
+      Location: "string",
+      Price: "string",
+      Cuisine: "string",
+      Longitude: "string",
+      Latitude: "string",
+      PhoneNumber: "string",
+      Url: "string",
+      WebsiteUrl: "string",
+      Award: "string",
+      FacilitiesAndServices: "string",
+      Description: "string",
+    },
+  });
+  await insertBatch(restaurantDB, data, { batchSize: 100 });
 }
 
 function jsonToHtmlTable(json) {
-    // Clear existing table
-    table.innerHTML = "";
-    if (!json[0]) return;
+  // Clear existing table
+  table.innerHTML = "";
+  if (!json[0]) return;
 
-    // Create table header
-    const ignoreList = [
-        "Longitude",
-        "Latitude",
-        "Url",
-        "WebsiteUrl",
-        "FacilitiesAndServices",
-    ];
-    const thead = table.createTHead();
-    let row = thead.insertRow();
-    let columns = Object.keys(json[0].document);
+  // Create table header
+  const ignoreList = [
+    "Longitude",
+    "Latitude",
+    "Url",
+    "WebsiteUrl",
+    "PhoneNumber",
+    "FacilitiesAndServices",
+    "Description",
+  ];
+  const thead = table.createTHead();
+  let row = thead.insertRow();
+  let columns = Object.keys(json[0].document);
+  for (let key of columns) {
+    // Skip keys that should be ignored
+    if (ignoreList.includes(key)) continue;
+
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+
+  // Create table body
+  const tbody = table.createTBody();
+  for (let element of json) {
+    row = tbody.insertRow();
     for (let key of columns) {
-        // Skip keys that should be ignored
-        if (ignoreList.includes(key)) continue;
+      // Skip keys that should be ignored
+      if (ignoreList.includes(key)) continue;
 
-        let th = document.createElement("th");
-        let text = document.createTextNode(key);
-        th.appendChild(text);
-        row.appendChild(th);
-    }
+      let cell = row.insertCell();
 
-    // Create table body
-    const tbody = table.createTBody();
-    for (let element of json) {
-        row = tbody.insertRow();
-        for (let key of columns) {
-            // Skip keys that should be ignored
-            if (ignoreList.includes(key)) continue;
-
-            let cell = row.insertCell();
-
-            // Add relevant attribute based on key.
-            // E.g. if the key is "Name", create a link element and set its href attribute
-            switch (key) {
-                case "Name": {
-                    const websiteUrl = element.document["WebsiteUrl"];
-                    if (!websiteUrl) {
-                        const text = document.createTextNode(
-                            element.document[key],
-                        );
-                        cell.appendChild(text);
-                        break;
-                    }
-                    const link = document.createElement("a");
-                    link.setAttribute("href", websiteUrl);
-                    link.innerText = element.document[key];
-                    cell.appendChild(link);
-                    break;
-                }
-                case "Address": {
-                    const address = element.document["Address"];
-                    if (address) {
-                        const link = document.createElement("a");
-                        link.setAttribute(
-                            "href",
-                            `https://maps.google.com/?q=${address}`,
-                        );
-                        link.setAttribute("target", "_blank");
-                        link.innerText = address;
-                        cell.appendChild(link);
-                    }
-                    break;
-                }
-                case "PhoneNumber": {
-                    const phoneNumber = element.document["PhoneNumber"];
-                    if (phoneNumber) {
-                        let text = document.createTextNode(
-                            element.document[key],
-                        );
-                        cell.appendChild(text);
-                    }
-                    break;
-                }
-                case "Award": {
-                    const link = document.createElement("a");
-                    const url = element.document["Url"];
-                    link.setAttribute("href", url);
-                    link.innerText = element.document[key];
-                    cell.appendChild(link);
-                    break;
-                }
-                default: {
-                    const text = document.createTextNode(element.document[key]);
-                    cell.appendChild(text);
-                    break;
-                }
-            }
+      // Add relevant attribute based on key.
+      // E.g. if the key is "Name", create a link element and set its href attribute
+      switch (key) {
+        case "Name": {
+          const websiteUrl = element.document["WebsiteUrl"];
+          if (!websiteUrl) {
+            const text = document.createTextNode(element.document[key]);
+            cell.appendChild(text);
+            break;
+          }
+          const link = document.createElement("a");
+          link.setAttribute("href", websiteUrl);
+          link.innerText = element.document[key];
+          cell.appendChild(link);
+          break;
         }
+        case "Address": {
+          const address = element.document["Address"];
+          if (address) {
+            const link = document.createElement("a");
+            link.setAttribute("href", `https://maps.google.com/?q=${address}`);
+            link.setAttribute("target", "_blank");
+            link.innerText = address;
+            cell.appendChild(link);
+          }
+          break;
+        }
+        case "PhoneNumber": {
+          const phoneNumber = element.document["PhoneNumber"];
+          if (phoneNumber) {
+            let text = document.createTextNode(element.document[key]);
+            cell.appendChild(text);
+          }
+          break;
+        }
+        case "Award": {
+          const link = document.createElement("a");
+          const url = element.document["Url"];
+          link.setAttribute("href", url);
+          link.innerText = element.document[key];
+          cell.appendChild(link);
+          break;
+        }
+        default: {
+          const text = document.createTextNode(element.document[key]);
+          cell.appendChild(text);
+          break;
+        }
+      }
     }
+  }
 }
 
 function handleSearch(event) {
-    const searchTerm = document.getElementById("search-term").value;
-    if (!searchTerm || !restaurantDB) {
-        summary.innerText = "";
-        table.innerHTML = "";
-        return;
-    }
+  const searchTerm = document.getElementById("search-term").value;
+  if (!searchTerm || !restaurantDB) {
+    summary.innerText = "";
+    table.innerHTML = "";
+    return;
+  }
 
-    const searchResult = search(restaurantDB, {
-        term: searchTerm,
-        properties: ["Name", "Address", "Location", "Cuisine"],
-        tolerance: 3,
-        limit: 20,
-    });
+  const searchResult = search(restaurantDB, {
+    term: searchTerm,
+    properties: ["Name", "Address", "Location", "Cuisine"],
+    tolerance: 3,
+    limit: 20,
+  });
 
-    summary.innerText = `Found ${
-        searchResult.count
-    } results. Search took ${formatNanoseconds(searchResult.elapsed)}.`;
-    jsonToHtmlTable(searchResult.hits);
+  summary.innerText = `Found ${
+    searchResult.count
+  } results. Search took ${formatNanoseconds(searchResult.elapsed)}.`;
+  jsonToHtmlTable(searchResult.hits);
 }
 
 window.onload = async function (event) {
-    await createLyraInstance(event);
-    handleSearch(event);
+  await createLyraInstance(event);
+  handleSearch(event);
 };
 
 document.body.addEventListener("input", handleSearch);
