ebd3779931a95c2c913534f1e36418c2a8971a85
Jerry Ng <ngshiheng@gmail.com>
2024-06-06 22:30:25 +0800

    refactor: remove green star from const

--- pkg/michelin/distinction.go
+++ pkg/michelin/distinction.go
@@ -5,6 +5,5 @@ const (
 	TwoStars            = "2 Stars"
 	OneStar             = "1 Star"
 	BibGourmand         = "Bib Gourmand"
-	GreenStar           = "Green Star"
 	SelectedRestaurants = "Selected Restaurants"
 )
