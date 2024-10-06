3dadd302e607efd6ad31a95922f316316bf8fe69
Jerry Ng <ngshiheng@gmail.com>
2024-10-06 16:31:00 +0800

    fix: remove github.com/carlmjohnson/versioninfo as dependency

--- cmd/mym/mym.go
+++ cmd/mym/mym.go
@@ -2,26 +2,50 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
-	"github.com/carlmjohnson/versioninfo"
+	"runtime/debug"
+
 	"github.com/ngshiheng/michelin-my-maps/v2/pkg/crawler"
 	log "github.com/sirupsen/logrus"
 )
 
+// printVersion prints the application version
+func printVersion() {
+	buildInfo, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("Unable to determine version information.")
+		return
+	}
+
+	if buildInfo.Main.Version != "" {
+		fmt.Printf("Version: %s\n", buildInfo.Main.Version)
+	} else {
+		fmt.Println("Version: unknown")
+	}
+}
+
+var (
+	logLevel    = flag.String("log", log.InfoLevel.String(), "log level (debug, info, warning, error, fatal, panic)")
+	helpFlag    = flag.Bool("help", false, "show help message")
+	versionFlag = flag.Bool("version", false, "print version information")
+)
+
 func main() {
-	var (
-		logLevel = flag.String("log", log.InfoLevel.String(), "log level (debug, info, warning, error, fatal, panic)")
-		helpFlag = flag.Bool("help", false, "show help message")
-	)
-	versioninfo.AddFlag(nil)
 	flag.Parse()
+
+	if *versionFlag {
+		printVersion()
+		return
+	}
 
 	if *helpFlag {
 		flag.Usage()
 		return
 	}
 
+	// Set log level
 	level, err := log.ParseLevel(*logLevel)
 	if err != nil {
 		log.Errorf("failed to parse log level: %v\n", err)
@@ -30,6 +54,7 @@ func main() {
 	log.SetLevel(level)
 	log.SetOutput(os.Stdout)
 
+	// Start crawling process
 	app := crawler.Default()
 	app.Crawl()
 }
